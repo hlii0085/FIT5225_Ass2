de9d87d56da4bf1204f63f473e6785d5be27ff8a
Daoyi-Zhang <dzha0025@student.monash.edu>
2020-06-05 23:51:20 +1000

    add comment

--- image_upload.py
+++ image_upload.py
@@ -2,23 +2,13 @@ import json
 import base64
 import boto3
 import time
-"""
-This model provides a mechanism to upload an image to an S3 bucket.
-    1. API Gateway or AWS SDKs(boto3)
-    2. Whenever an image is uploaded to the S3 bucket,
-       the system must trigger an event and thus invoke a Lambda function to do object detection
-    3. The Lambda function should read the uploaded image and detect objects in the image
-    4. Finally, save the list of detected objects along with the S3-url in an AWS database
-"""
-
-
-"""
-This function is the lambda function for uploading the image via API gateway
-When a http POST request with the image in its body is sent to the gateway it will called this function
-"""
 
 
 def lambda_handler(event, context):
+    """
+    This function is the lambda function for uploading the image via API gateway
+    When a http POST request with the image in its body is sent to the gateway it will called this function
+    """
     s3 = boto3.client("s3")
 
     image = event["content"]
