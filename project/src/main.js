5ce43b5f242df4e6eaeef4647327234aac8a8bb1
Yichi-Zhang1 <yzha0410@student.monash.edu>
2020-06-08 10:00:53 +0800

    test

--- project/src/main.js
+++ project/src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
 
 Vue.config.productionTip = false
 
