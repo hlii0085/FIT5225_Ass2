5521614e26196dc0a44671534d911058046cf3d5
Yichi-Zhang1 <yzha0410@student.monash.edu>
2020-06-08 22:39:02 +0800

    test manual and continuous deploy

--- project/src/main.js
+++ project/src/main.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Amplify from 'aws-amplify';
-import '@aws-amplify/ui-vue';
-import aws_exports from './aws-exports';
-
-Amplify.configure(aws_exports);
 
 Vue.config.productionTip = false
 
