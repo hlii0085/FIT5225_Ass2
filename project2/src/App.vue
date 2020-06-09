98d829609607d6b98a4085f952ddb8ea5f650bea
Yichi-Zhang1 <yzha0410@student.monash.edu>
2020-06-09 11:51:51 +0800

    complete hosted UI

--- project2/src/App.vue
+++ project2/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <amplify-authenticator>
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App111"/>
+      <amplify-sign-out></amplify-sign-out>
+    </div>
+  </amplify-authenticator>
 </template>
+
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
--- project2/src/main.js
+++ project2/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+//build in UI
+import '@aws-amplify/ui-vue';
+Amplify.configure(awsconfig);
+
 Vue.config.productionTip = false
 
 new Vue({
